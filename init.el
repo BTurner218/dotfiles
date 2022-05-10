a3b308d76065fb9cee525d2b3473ee3c72ac8db8
Brandon Turner <bturner218@outlook.com>
2022-05-09 22:18:22 -0400

    update emacs config

--- init.el
+++ init.el
@@ -18,13 +18,14 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(setq scroll-conservatively 101)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(kaolin-themes evil)))
+ '(package-selected-packages '(ivy kaolin-themes evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -43,3 +44,8 @@
   :config
   (load-theme 'kaolin-dark t)
   (kaolin-treemacs-theme))
+
+;; Ivy Config
+(ivy-mode)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
