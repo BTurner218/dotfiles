dc9e4d6332d03b1f0b0ec7beabc945e82ab2fdda
Brandon Turner <bturner218@outlook.com>
2022-05-09 22:04:00 -0400

    Add emacs config

---
+++ init.el
@@ -0,0 +1,45 @@
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+
+;; General Settings
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(kaolin-themes evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'use-package)
+(require 'kaolin-themes)
+(load-theme 'kaolin-dark t)
+;; Apply treemacs customization for Kaolin themes, requires the all-the-icons package.
+(kaolin-treemacs-theme)
+
+;; Or if you have use-package installed
+(use-package kaolin-themes
+  :config
+  (load-theme 'kaolin-dark t)
+  (kaolin-treemacs-theme))
