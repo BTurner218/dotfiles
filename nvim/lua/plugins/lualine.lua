ee1b8e471c8ae6fa82fd46e8fef616d47971dc0a
Brandon Turner <bturner218@outlook.com>
2022-06-10 09:55:01 -0400

    removed filename from lualine tabs

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ require('lualine').setup {
   tabline = {
     lualine_a = {'buffers'},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {},
     lualine_x = {},
     lualine_y = {},
     lualine_z = {'tabs'}
