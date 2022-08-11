4ddf62342e6c5161c35c07a4c76e6e9efce3f8c0
Brandon Turner <bturner218@outlook.com>
2022-08-10 22:31:10 -0400

    Added prisma to treesitter

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -34,7 +34,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver', 'gopls', 'eslint' }
+local servers = { 'tsserver', 'gopls', 'eslint', 'prismals' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
