46b00d1459da20f1c52a395009bcda9d83802bae
Brandon Turner <bturner218@outlook.com>
2022-12-06 20:06:55 -0500

    Add tailwindcss to lsp config

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -65,7 +65,7 @@ lspconfig.tsserver.setup {
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'gopls', 'eslint', 'prismals' }
+local servers = { 'gopls', 'eslint', 'prismals', 'tailwindcss' }
 for _, lsp in pairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
