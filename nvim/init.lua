51009b57a14663d59c3e4283742da00b307c3cf6
Brandon Turner <bturner218@outlook.com>
2023-03-02 19:32:02 -0500

    Use lsp-zero

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,15 +7,12 @@ Plug('nvim-lualine/lualine.nvim')
 Plug('kyazdani42/nvim-web-devicons')
 Plug('kyazdani42/nvim-tree.lua')
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
-Plug('neovim/nvim-lspconfig')
 Plug ('akinsho/toggleterm.nvim', {branch = 'main'})
 Plug('airblade/vim-gitgutter')
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim')
 Plug('fatih/vim-go', {['do'] = ':GoUpdateBinaries'})
 Plug('sbdchd/neoformat')
-Plug('hrsh7th/nvim-cmp')
-Plug('hrsh7th/cmp-nvim-lsp')
 Plug('saadparwaiz1/cmp_luasnip')
 Plug('L3MON4D3/LuaSnip')
 Plug('tpope/vim-fugitive')
@@ -27,6 +24,27 @@ Plug('prettier/vim-prettier', {['do'] = 'yarn install --frozen-lockfile --produc
 Plug('github/copilot.vim')
 Plug('mbbill/undotree')
 
+-- lsp-zero
+-- LSP Support
+Plug('neovim/nvim-lspconfig')              -- Required
+Plug('williamboman/mason.nvim')            --Optional
+Plug('williamboman/mason-lspconfig.nvim')  -- Optional
+
+-- Autocompletion Engine
+Plug('hrsh7th/nvim-cmp')         -- Required
+Plug('hrsh7th/cmp-nvim-lsp')     -- Required
+Plug('hrsh7th/cmp-buffer')       -- Optional
+Plug('hrsh7th/cmp-path')         -- Optional
+Plug('saadparwaiz1/cmp_luasnip') -- Optional
+Plug('hrsh7th/cmp-nvim-lua')     -- Optional
+
+--  Snippets
+Plug('L3MON4D3/LuaSnip')             -- Required
+Plug('rafamadriz/friendly-snippets') -- Optional
+
+Plug('VonHeikemen/lsp-zero.nvim', {branch = 'v1.x'})
+
+
 vim.call('plug#end')
 
 require("core/keymaps")
@@ -35,11 +53,10 @@ require("core/colors")
 require("plugins/lualine")
 require("plugins/nvim-tree")
 require("plugins/nvim-treesitter")
-require("plugins/nvim-lspconfig")
 require("plugins/toggleterm")
 require("plugins/neoformat")
-require("plugins/nvim-cmp")
 require('plugins/hop')
 require('plugins/vim-prettier')
 require('plugins/undotree')
+require('plugins/lsp-zero')
 
